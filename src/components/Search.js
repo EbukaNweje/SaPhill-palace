cd0460cc7537f6d394d89431d6dfdad868279352
EbukaNweje <nwejeebuka@gmail.com>
2022-09-19 15:24:42 +0100

    Search Fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,7 +27,7 @@ const Search = () => {
       <h3> {props.name} </h3>
       <h5> ₦ {props.price} </h5>
   </div>
-        )): "No item mearch"
+        )): "No item Match"
        }
     </div>
   )
